4b3381d31cb3fda7e562706bd7b2fa6ca8f18c51
pilotocheg <slavutich.org@gmail.com>
2018-04-28 00:54:30 +0300

    lesson 10 ok

---
+++ lesson_10/index.js
@@ -0,0 +1,21 @@
+const myAge = 1102;
+
+var year = ('лет');
+
+if ((myAge % 10) == 1) {
+	year = ('год')
+}
+
+if (myAge >= 5 & myAge <= 20) { 
+	year = ('лет')
+}
+
+if ((myAge % 100) >= 5 & (myAge % 100) <=20) {
+	year = ('лет')
+}
+
+else if ((myAge % 10) >= 2 & (myAge % 10) <= 4) {
+	year = ('года')
+}
+
+console.log(`Мой возраст ${myAge} ${year}`);
