abf07b555264de7e0397d6f9b97fc9a7206dd9c8
pilotocheg <slavutich.org@gmail.com>
2018-04-28 15:08:51 +0300

    script "years" fixed

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -1,21 +1,17 @@
-const myAge = 1102;
+const myAge = 115;
 
-var year = ('лет');
+let year = 'лет';
 
 if ((myAge % 10) == 1) {
-	year = ('год')
+	year = 'год'
 }
 
-if (myAge >= 5 & myAge <= 20) { 
-	year = ('лет')
+if ((myAge % 100) >= 5 && (myAge % 100) <=20) {
+	year = 'лет'
 }
 
-if ((myAge % 100) >= 5 & (myAge % 100) <=20) {
-	year = ('лет')
-}
-
-else if ((myAge % 10) >= 2 & (myAge % 10) <= 4) {
-	year = ('года')
+else if ((myAge % 10) >= 2 && (myAge % 10) <= 4) {
+	year = 'года'
 }
 
 console.log(`Мой возраст ${myAge} ${year}`);
