2b73116a2e16b8cb04023ee22c9a2a541a12c1f5
pilotocheg <slavutich.org@gmail.com>
2018-05-12 23:47:53 +0300

    lesson 13 first part

---
+++ lesson_13/index.js
@@ -0,0 +1,23 @@
+function removeKeys1(obj1, arr1) {
+	for (let key of arr1) {
+		if (key in obj1) {
+			delete obj1[key];
+		}
+	}
+	return obj1;
+}
+
+function removeKeys2(obj2, arr2) {
+	for (let key of arr2) {
+		if (key in obj2) {
+			delete obj2[key];
+		}
+	}
+	let newObj = new Object(obj2);
+	
+	return newObj;
+}
+
+function absDiff(a) {
+	return (b) => Math.abs(a - b);
+}
