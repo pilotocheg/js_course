7aa39d088bf3fbf04e92fe5ae94af03acf748bb3
pilotocheg <slavutich.org@gmail.com>
2018-05-21 22:54:44 +0300

    lesson 15 ok

---
+++ lesson_15/index.js
@@ -0,0 +1,27 @@
+const parentEl = document.body;
+
+function init(arr, num) {
+	let list = parentEl.appendChild(document.createElement('ul'));
+
+	for (i = 0; i < num; i++) {
+		let listLi = list.appendChild(document.createElement('li'));
+		listLi.className = arr.className;
+
+		for (key in arr.attributes) {
+			listLi.setAttribute(key, arr.attributes[key])
+		}
+		listLi.innerHTML = arr.content;
+	}
+}
+
+function listen() {
+	let list = document.querySelector('ul');
+	if (list) {
+		document.body.removeChild(list);
+		return console.log(true);
+	} else {
+		return console.log(false);
+	}
+}
+
+document.querySelector('button').addEventListener('click', listen);
