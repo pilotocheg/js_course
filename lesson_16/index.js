20c1574129037ba8cd1529a8c576e9a3a9d97e59
pilotocheg <slavutich.org@gmail.com>
2018-05-23 19:31:36 +0300

    timer v1 ok

--- lesson_16/index.js
+++ lesson_16/index.js
@@ -1,11 +1,15 @@
-const n = 10;
+const n = 15;
+
 const numbers = document.getElementById('numbers');
 const startB = document.getElementById('start');
 const pauseB = document.getElementById('pause');
 const stopB = document.getElementById('stop');
 const second = document.getElementById('second');
-let num = 0;
-let speed = 1000;
+const letS = document.getElementById('letS');
+
+let num = 0, counter = 0, speed = 1000, interval;
+
+letS.innerHTML = 's';
 
 second.appendChild(document.createTextNode(num));
 
@@ -20,11 +24,35 @@ for (i = 1; i <= n; i += 1) {
 	}
 }
 
+
 function count() {
-	let interval = setInterval(function(){
+	
+	interval = setInterval(function(){
 		num += 1;
 		second.innerHTML = num;
 		startB.setAttribute('disabled', 'true');
+
+		if (num === 1) {
+			letS.innerHTML = '';
+		} else {
+			letS.innerHTML = 's';
+		}
+
+		if (counter < n) {
+			counter += 1;
+			document.getElementById(`span_${counter}`).style.backgroundColor = 'orange';
+		} else if (counter === n) {
+			counter += n + 1;
+			document.getElementById(`span_${counter - n - 1}`).style.backgroundColor = 'gray';
+			counter -= 1;
+		} else if (counter > n + 1) {
+			counter -= 1;
+			document.getElementById(`span_${counter - n}`).style.backgroundColor = 'gray';
+		} else if (counter === n + 1) {
+			counter = 0;
+			document.getElementById(`span_${counter + 1}`).style.backgroundColor = 'orange';
+			counter += 1;
+		}
 	}, speed);
 
 	pauseB.addEventListener('click', function(){
@@ -36,17 +64,32 @@ function count() {
 		clearInterval(interval);
 		second.innerHTML = 0;
 		num = 0;
+		counter = 0;
+		
+		let numb = numbers.children;
+		for (let i = 1; i < numb.length - 1; i++ ) {
+			numb[i].style.backgroundColor = '';
+		}
+
 		startB.removeAttribute('disabled');
+		letS.innerHTML = 's';
 	})
 }
 
 startB.addEventListener('click', count);
+
 document.getElementById('x1').addEventListener('click', function(){
 	speed = 1000;
+	clearInterval(interval);
+	startB.removeAttribute('disabled');
 });
 document.getElementById('x2').addEventListener('click', function(){
 	speed = 500;
+	clearInterval(interval);
+	startB.removeAttribute('disabled');
 });
 document.getElementById('x3').addEventListener('click', function(){
 	speed = 366;
+	clearInterval(interval);
+	startB.removeAttribute('disabled');
 })
