1115ac467f87ab9398fe6bedd630c0e4baed4743
pilotocheg <slavutich.org@gmail.com>
2018-05-25 19:27:08 +0300

    task 2 ok

--- module/index.js
+++ module/index.js
@@ -4,3 +4,19 @@ function itsMe(a = 'pilotocheg'){
 	console.log(`It's me ${a}`);
 	return `It's me ${a}`;
 }
+
+// Task 2
+
+function compare(a, b){
+	if (typeof a !== 'number' || typeof b !== 'number'){
+		return 'НЕ ЧИСЛО';
+	} else {
+		if (a > b) {
+			return `${a} > ${b}`;
+		} else if (a < b) {
+			return `${a} < ${b}`;
+		} else if (a === b) {
+			return `${a} == ${b}`;
+		} 
+	}
+}
