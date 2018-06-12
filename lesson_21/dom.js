6bd3c999f25040ad2705fb0fb023c612021c24e2
pilotocheg <slavutich.org@gmail.com>
2018-06-12 20:37:40 +0300

    problems fixed

--- lesson_21/dom.js
+++ lesson_21/dom.js
@@ -1,35 +1,35 @@
 export default class Dom {
     constructor() {
-        const lat = document.getElementById('lat');
-        const long = document.getElementById('long');
-        const city = document.getElementById('city');
-        const country = document.getElementById('country');
-        const ip = document.getElementById('ip');
+        this.lat = document.getElementById('lat');
+        this.long = document.getElementById('long');
+        this.city = document.getElementById('city');
+        this.country = document.getElementById('country');
+        this.ip = document.getElementById('ip');
 
-        const hover = document.getElementById('hover');
-        const animation = document.getElementById('animation');
-        const circle1 = document.getElementById('circle1');
-        const circle2 = document.getElementById('circle2');
-        const circle3 = document.getElementById('circle3');
+        this.hover = document.getElementById('hover');
+        this.animation = document.getElementById('animation');
+        this.circle1 = document.getElementById('circle1');
+        this.circle2 = document.getElementById('circle2');
+        this.circle3 = document.getElementById('circle3');
     }
 
     showPreloader() {
-        hover.style.display = 'block';
-        animation.style.display = 'block';
+        this.hover.style.display = 'block';
+        this.animation.style.display = 'block';
         this.i = 2;
         this.interval = setInterval(() => {
             switch (this.i) {
                 case 1:
-                    circle1.style.background = 'darkmagenta';
-                    circle3.style.background = 'orange';
+                    this.circle1.style.background = 'darkmagenta';
+                    this.circle3.style.background = 'orange';
                     break;
                 case 2:
-                    circle1.style.background = 'orange';
-                    circle2.style.background = 'darkmagenta';
+                    this.circle1.style.background = 'orange';
+                    this.circle2.style.background = 'darkmagenta';
                     break;
                 case 3:
-                    circle2.style.background = 'orange';
-                    circle3.style.background = 'darkmagenta';
+                    this.circle2.style.background = 'orange';
+                    this.circle3.style.background = 'darkmagenta';
                     this.i = 0;
                     break;
             }
@@ -38,21 +38,21 @@ export default class Dom {
     };
 
     hidePreloader() {
-        hover.style.display = 'none';
-        animation.style.display = 'none';
+        this.hover.style.display = 'none';
+        this.animation.style.display = 'none';
         clearInterval(this.interval);
         this.i = 2;
-        circle1.style.background = 'darkmagenta';
-        circle2.style.background = 'orange';
-        circle3.style.background = 'orange';
+        this.circle1.style.background = 'darkmagenta';
+        this.circle2.style.background = 'orange';
+        this.circle3.style.background = 'orange';
 
     };
 
     setCoordinates(crd) {
-        lat.value = crd.latitude;
-        long.value = crd.longitude;
-        city.value = crd.city;
-        country.value = crd.country_name;
-        ip.innerHTML = `ip: ${crd.ip}`;
+        this.lat.value = crd.latitude;
+        this.long.value = crd.longitude;
+        this.city.value = crd.city;
+        this.country.value = crd.country_name;
+        this.ip.innerHTML = `ip: ${crd.ip}`;
     };
 }
