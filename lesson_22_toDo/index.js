21c422fb848c67dfdb5fea988bb524b2be073bf6
pilotocheg <slavutich.org@gmail.com>
2018-06-15 00:12:23 +0300

    todo list fixed

--- lesson_22_toDo/index.js
+++ lesson_22_toDo/index.js
@@ -1,37 +1,49 @@
-const textInp = document.getElementById('text');
-const list = document.getElementById('list');
+class Task {
+    constructor () {
+        this.list = document.getElementById('list');
+        this.text = document.getElementById('text');
+
+        this.li = document.createElement('li');
+        this.del = document.createElement('button');
+        this.done = document.createElement('label');
+        this.input = document.createElement('input');
+
+        this.createTask();
+    }
+
+    createTask() {
+        this.done.setAttribute('class', 'done');
+        this.done.innerHTML = 'v';
+        this.input.type = 'checkbox';
+        this.input.onchange = this.doneTask.bind(this);
+        this.done.appendChild(this.input);
+        this.del.setAttribute('class', 'delete');
+        this.del.innerHTML = 'x';
+        this.del.onclick = this.deleteTask.bind(this);
+
+        this.li.innerHTML = this.text.value;
+        this.li.appendChild(this.done);
+        this.li.appendChild(this.del);
+        this.list.appendChild(this.li);
+    };
+
+    deleteTask() {
+        this.li.remove();
+        delete this;
+    }
+    doneTask() {
+        if(this.input.checked) {
+            this.li.style.textDecoration = 'line-through';
+            this.done.style.background = 'lightseagreen';
+        } else {
+            this.li.style.textDecoration = 'none';
+            this.done.style.background = 'none';
+        }
+    }
+}
+
 const btn = document.getElementById('btn');
 
-function createTask() {
-    const value = textInp.value;
-    if (value) {
-        let li = document.createElement('li');
-        let del = document.createElement('button');
-        let done = document.createElement('label');
-        let check = document.createElement('input');
-
-        li.innerHTML = value;
-        del.setAttribute('class', 'delete');
-        del.setAttribute('onclick', 'parentNode.remove()');
-        del.innerHTML = 'x';
-        
-        done.setAttribute('class', 'done');
-        done.innerHTML = 'v';
-        
-        check.setAttribute('type', 'checkbox');
-        check.setAttribute('checked', 'false');
-        check.setAttribute('onclick', 'if(this.checked) {parentNode.parentNode.style.textDecoration = "none"} else {parentNode.parentNode.style.textDecoration = "line-through"}');
-
-        done.appendChild(check);
-        li.appendChild(del);
-        li.appendChild(done);
-        list.appendChild(li);
-        textInp.value = '';
-    }
-};
-
-btn.addEventListener('click', createTask);
-// window.onbeforeunload = function() {
-//     localStorage.setItem('list', `${JSON.stringify(list)}`);
-// }
-// window.onload = localStorage.getItem('list');
+btn.addEventListener('click', () => {
+    new Task();
+})
