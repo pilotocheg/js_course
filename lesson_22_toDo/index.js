6a2e3ae37f3f815a9740a11a7e088d358b117113
pilotocheg <slavutich.org@gmail.com>
2018-06-15 22:00:20 +0300

    todo fixed

--- lesson_22_toDo/index.js
+++ lesson_22_toDo/index.js
@@ -7,6 +7,7 @@ class Task {
         this.del = document.createElement('button');
         this.done = document.createElement('label');
         this.input = document.createElement('input');
+        this.counter = counter;
 
         this.createTask();
     }
@@ -27,32 +28,64 @@ class Task {
         this.list.appendChild(this.li);
         
         this.text.value = '';
+        counter++;
     };
 
     deleteTask() {
         this.li.remove();
-        delete this;
-        console.log(this);
+        delete taskArr[this.counter];
+        if(this.list.children.length === 0) {
+            while (taskArr.length) {
+                taskArr.splice(0, 1);
+                counter = 0;
+            }
+        }
+        console.log(taskArr);
     }
     doneTask() {
         if(this.input.checked) {
             this.li.style.textDecoration = 'line-through';
             this.li.style.color = 'lightseagreen';
+            this.li.style.background = 'lightgray';
             this.done.style.background = 'lightseagreen';
         } else {
             this.li.style.textDecoration = 'none';
             this.li.style.color = 'lightcoral';
+            this.li.style.background = 'white';
             this.done.style.background = 'none';
         }
     }
 }
 
 const btn = document.getElementById('btn');
+const taskArr = [];
+let counter = 0;
 
 btn.addEventListener('click', () => {
     const text = document.getElementById('text');
     if (text.value) {
-        new Task();
+        taskArr.push(new Task());
+        console.log(taskArr);
     }
     
 })
+
+window.onbeforeunload = () => {
+    if(taskArr.length) {
+        const jsonArr = JSON.stringify(taskArr);
+        window.localStorage.setItem('taskArr', jsonArr);
+    }
+}
+
+window.onload = () => {
+    if(window.localStorage.length) {
+        const list = document.getElementById('list');
+        console.log(window.localStorage);
+        let jsonArr = JSON.parse(window.localStorage.getItem('taskArr'));
+        console.log(jsonArr);
+        taskArr.concat(jsonArr);
+        for (let i in taskArr) {
+            list.appendChild(taskArr[i].li);
+        }
+    }
+}
